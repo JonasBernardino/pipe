234908c9b128716f67978e361336ce82151f0f2a
jonasbernardino <jonasl.bernardino@gmail.com>
2026-05-19 17:12:52 -0300

    qualidade

---
+++ src/main/java/com/pie/pipe/service/UsuarioService.java
@@ -0,0 +1,14 @@
+package com.pie.pipe.service;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioService {
+
+    public int calcularTamanhoDoNome() {
+        String nome = null;
+
+        return nome.length();
+    }
+}
